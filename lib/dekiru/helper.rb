7b9c9e7dceee9181030d086cea93887fef45051c
Akihiro Matsumura <matsumura.aki@gmail.com>
2012-07-20 16:22:09 +0900

    html_safe for share_tags

--- lib/dekiru/helper.rb
+++ lib/dekiru/helper.rb
@@ -19,21 +19,24 @@ module Dekiru
     end
 
     def facebook_like(url = url_for(only_path: false))
-      %Q(<iframe src="//www.facebook.com/plugins/like.php?href=#{u(url)}&amp;send=false&amp;layout=button_count&amp;width=140&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21&amp;appId=244243155670810" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:140px; height:21px;" allowTransparency="true"></iframe>)
+      html = %Q(<iframe src="//www.facebook.com/plugins/like.php?href=#{u(url)}&amp;send=false&amp;layout=button_count&amp;width=140&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21&amp;appId=244243155670810" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:140px; height:21px;" allowTransparency="true"></iframe>)
+      html.html_safe
     end
 
     def twitter_tweet
-      <<-EOF
+      html = <<-EOF
 <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 EOF
+      html.html_safe
     end
 
     def google_plus
-      <<-EOF
+      html = <<-EOF
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 <g:plusone size="medium"></g:plusone>
 EOF
+      html.html_safe
     end
   end
 end
