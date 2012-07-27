a55cc04035039902ee40bb6ff59447b83835964e
interu <beautybeast07@gmail.com>
2012-07-27 11:38:38 +0900

    add google analytics

--- lib/dekiru/helper.rb
+++ lib/dekiru/helper.rb
@@ -38,5 +38,24 @@ EOF
 EOF
       html.html_safe
     end
+
+    def google_analytics(code = nil)
+      if code && ::Rails.env == "production"
+        html = <<-EOF
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '#{code}']);
+    _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+  </script>
+EOF
+        html.html_safe
+      end
+    end
   end
 end
