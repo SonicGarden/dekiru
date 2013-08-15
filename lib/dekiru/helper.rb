e544caa8f6d706d6168b0000e89dfcaede95c886
bomcat <bomcat.k@gmail.com>
2013-08-15 16:36:52 +0900

    Update Tweet button

--- lib/dekiru/helper.rb
+++ lib/dekiru/helper.rb
@@ -27,7 +27,7 @@ module Dekiru
     def twitter_tweet
       html = <<-EOF
 <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 EOF
       html.html_safe
     end
