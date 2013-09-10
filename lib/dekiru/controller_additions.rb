73690e7abe1f340ecd3974e53200be580c61979b
Akihiro Matsumura <matsumura.aki@gmail.com>
2013-09-10 16:33:01 +0900

    Use http_accesept_language 2.0

--- lib/dekiru/controller_additions.rb
+++ lib/dekiru/controller_additions.rb
@@ -15,11 +15,5 @@ module Dekiru
       logger.debug "[debug] #{http_accept_language.user_preferred_languages}"
       http_accept_language.compatible_language_from(I18n.available_locales)
     end
-
-    # HttpAcceptLanguage::EasyAccess is include to ApplicationController
-    # so, no method error occured
-    def http_accept_language
-      env.http_accept_language
-    end
   end
 end
