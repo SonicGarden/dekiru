f9c53acbd03e03ec0c1d7ddaf2f825f07ec2a954
Akihiro Matsumura <matsumura.aki@gmail.com>
2014-05-01 13:18:14 +0900

    Add null check localization helper method

--- lib/dekiru/helper.rb
+++ lib/dekiru/helper.rb
@@ -18,6 +18,11 @@ module Dekiru
       end
     end
 
+    def null_check_localization(time)
+      localize(time) if time.present?
+    end
+    alias nl null_check_localization
+
     def facebook_like(url = url_for(only_path: false), app_id = nil, width = 140)
       facebook_app_id = app_id || ENV["FACEBOOK_APP_ID"]
       html = %Q(<iframe src="//www.facebook.com/plugins/like.php?href=#{u(url)}&amp;send=false&amp;layout=button_count&amp;width=140&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21&amp;appId=#{facebook_app_id}" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:#{width}px; height:21px;" allowTransparency="true"></iframe>)
