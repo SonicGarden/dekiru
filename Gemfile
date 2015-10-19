f7135e03fe259fe9f31907ba7c79e5a33a8a9410
kihara <kihara@pltokyo.com>
2015-10-19 11:05:05 +0900

    gem指定を変更

--- Gemfile
+++ Gemfile
@@ -2,4 +2,3 @@ source 'https://rubygems.org'
 
 # Specify your gem's dependencies in dekiru.gemspec
 gemspec
-gem 'haml-rails'
--- app/mailers/smtp_check_mailer.rb
+++ app/mailers/smtp_check_mailer.rb
@@ -1,9 +1,9 @@
 class SmtpCheckMailer < ActionMailer::Base
   default from: "smtp-checker@sonicgarden.jp"
 
-  def checkmail(app_key)
-    @app_key = app_key
-    subject = "[SMTP Checker] メール送信チェック"
+  def checkmail
+    @app_key = ENV['SMTP_CHECKER_APP_KEY']
+    subject = "[SMTP Checker] SMTP Check Mail"
 
     mail to: ENV['SMTP_CHECKER_TO_ADDR'], subject: subject
   end
