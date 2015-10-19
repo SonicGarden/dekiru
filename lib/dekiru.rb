8195e98dc5183ff6a639b73a78afa9675c1cb55e
kihara <kihara@pltokyo.com>
2015-10-19 13:19:19 +0900

    task実行できないエラー対応

--- lib/dekiru.rb
+++ lib/dekiru.rb
@@ -2,7 +2,6 @@ require "dekiru/version"
 require "dekiru/railtie" if defined?(::Rails)
 require "dekiru/helper"
 require "dekiru/controller_additions"
-require File.expand_path('../../app/mailers',        __FILE__)
 
 module Dekiru
   class Railtie < ::Rails::Railtie
