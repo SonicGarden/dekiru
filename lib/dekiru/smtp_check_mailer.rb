abc2a97cbf4593714aa23538185102c8d670808a
kihara <kihara@pltokyo.com>
2015-10-26 13:18:20 +0900

    module配下に

--- lib/dekiru/smtp_check_mailer.rb
+++ lib/dekiru/smtp_check_mailer.rb
@@ -1,17 +1,21 @@
-class SmtpCheckMailer < ActionMailer::Base
-  default from: "smtp-checker@sonicgarden.jp"
-  def checkmail
-    app_key = ENV['SMTP_CHECKER_APP_KEY'] || Rails.application.class.parent_name
-    smtp_checker_to_addr = ENV['SMTP_CHECKER_TO_ADDR'] || 'smtp-check@sg-ops-mail.sonicgarden.jp'
+module Dekiru
+  class SmtpCheckMailer < ActionMailer::Base
+    default from: "smtp-checker@sonicgarden.jp"
 
-    Rails.logger.error "ENV['SMTP_CHECKER_APP_KEY'] undefined!" if app_key.blank?
-    Rails.logger.error "ENV['SMTP_CHECKER_TO_ADDR'] undefined!" if smtp_checker_to_addr.blank?
+    def checkmail
+      app_key = ENV['SMTP_CHECKER_APP_KEY'] || Rails.application.class.parent_name
+      smtp_checker_to_addr = ENV['SMTP_CHECKER_TO_ADDR'] || 'smtp-check@sg-ops-mail.sonicgarden.jp'
 
-    Rails.logger.info "checkmail send start #{smtp_checker_to_addr} key:#{smtp_checker_to_addr}"
-    subject = "[SMTP Checker] SMTP Check Mail"
-    mail to: smtp_checker_to_addr, subject: subject do |format|
-      format.text { render plain: app_key }
+      Rails.logger.error "ENV['SMTP_CHECKER_APP_KEY'] undefined!" if app_key.blank?
+      Rails.logger.error "ENV['SMTP_CHECKER_TO_ADDR'] undefined!" if smtp_checker_to_addr.blank?
+
+      Rails.logger.info "checkmail send start #{smtp_checker_to_addr} key:#{smtp_checker_to_addr}"
+      subject = "[SMTP Checker] SMTP Check Mail"
+      mail to: smtp_checker_to_addr, subject: subject do |format|
+        format.text { render plain: app_key }
+      end
+      Rails.logger.info "send to #{smtp_checker_to_addr} key:#{smtp_checker_to_addr}"
     end
-    Rails.logger.info "send to #{smtp_checker_to_addr} key:#{smtp_checker_to_addr}"
   end
 end
+
--- lib/dekiru/tasks/smtpcheck.rake
+++ lib/dekiru/tasks/smtpcheck.rake
@@ -2,6 +2,6 @@ desc 'smtp check'
 task sendmail_smtpcheck: :environment do
   Rails.logger.info "[INFO] Start smtpcheck:sendmail_smtpcheck env:#{Rails.env}"
 
-  SmtpCheckMailer.checkmail.deliver
+  Dekiru::SmtpCheckMailer.checkmail.deliver
   Rails.logger.info "[INFO] End smtpcheck:sendmail_smtpcheck env:#{Rails.env}"
 end
