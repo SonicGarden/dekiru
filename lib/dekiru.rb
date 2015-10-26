ddce1103dd16ed8a28220c2c4c0a8f9217dad03d
kihara <kihara@pltokyo.com>
2015-10-26 13:33:10 +0900

    requireでclass load

--- lib/dekiru.rb
+++ lib/dekiru.rb
@@ -2,6 +2,7 @@ require "dekiru/version"
 require "dekiru/railtie" if defined?(::Rails)
 require "dekiru/helper"
 require "dekiru/controller_additions"
+require "dekiru/smtp_check_mailer"
 
 module Dekiru
   class Railtie < ::Rails::Railtie
