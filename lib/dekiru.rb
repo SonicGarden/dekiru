cb8556ffd035c19f8512e4544b38bf099e4c32a5
Akihiro MATSUMURA <matsumura.aki@gmail.com>
2015-10-28 14:35:18 +0900

    Refactor smtp check

--- lib/dekiru.rb
+++ lib/dekiru.rb
@@ -2,12 +2,11 @@ require "dekiru/version"
 require "dekiru/railtie" if defined?(::Rails)
 require "dekiru/helper"
 require "dekiru/controller_additions"
-require "dekiru/smtp_check_mailer"
 
 module Dekiru
   class Railtie < ::Rails::Railtie
     rake_tasks do
-      load "dekiru/tasks/smtpcheck.rake"
+      load "dekiru/tasks/smtp_check.rake"
     end
   end
 end
--- lib/dekiru/tasks/smtpcheck.rake
+++
@@ -1,7 +0,0 @@
-desc 'smtp check'
-task sendmail_smtpcheck: :environment do
-  Rails.logger.info "[INFO] Start smtpcheck:sendmail_smtpcheck env:#{Rails.env}"
-
-  Dekiru::SmtpCheckMailer.checkmail.deliver
-  Rails.logger.info "[INFO] End smtpcheck:sendmail_smtpcheck env:#{Rails.env}"
-end
