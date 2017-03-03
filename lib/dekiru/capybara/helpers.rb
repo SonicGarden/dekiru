4135a6460ff10b88929920c3e891560321900ce4
Shunichi Ikegami <sike.tm@gmail.com>
2017-03-03 14:54:03 +0900

    Add comment

--- lib/dekiru/capybara/helpers.rb
+++ lib/dekiru/capybara/helpers.rb
@@ -13,6 +13,7 @@ module Dekiru
         end
       end
 
+      # https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
       def wait_for_ajax
         Timeout.timeout(::Capybara.default_max_wait_time) do
           loop until finished_all_ajax_requests?
