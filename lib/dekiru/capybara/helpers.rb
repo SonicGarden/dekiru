e13dd3efea4a8f2247e4d19bdc9b1e1bb111573e
aki <lala.akira@gmail.com>
2018-05-17 23:21:31 +0900

    feat: Add wait_until capybara helper

--- lib/dekiru/capybara/helpers.rb
+++ lib/dekiru/capybara/helpers.rb
@@ -13,29 +13,37 @@ module Dekiru
           })();
         EOS
         yield
-        Timeout.timeout(::Capybara.default_max_wait_time) do
-          script = <<~"EOS"
-            (function(){
-              var eventName = '#{event}';
-              return window._dekiruCapybaraWaitEvents && window._dekiruCapybaraWaitEvents[eventName];
-            })();
-          EOS
-          begin
-            result = page.evaluate_script(script)
-            raise Error, 'wait_for_event: Missing context. probably moved to another page.' if result.nil?
-          end while result == 1
+
+        script = <<~"EOS"
+          (function(){
+            var eventName = '#{event}';
+            return window._dekiruCapybaraWaitEvents && window._dekiruCapybaraWaitEvents[eventName];
+          })();
+        EOS
+        wait_until do
+          result = page.evaluate_script(script)
+          raise Error, 'wait_for_event: Missing context. probably moved to another page.' if result.nil?
+          result == 0
         end
       end
 
       # https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
       def wait_for_ajax
-        Timeout.timeout(::Capybara.default_max_wait_time) do
-          loop until finished_all_ajax_requests?
-        end
+        wait_until { finished_all_ajax_requests? }
       end
 
       def finished_all_ajax_requests?
         page.evaluate_script('jQuery.active').zero?
+      end
+
+      def wait_until(timeout: ::Capybara.default_max_wait_time, interval: 0.2, **opts, &block)
+        if defined?(Selenium::WebDriver::Wait)
+          Selenium::WebDriver::Wait.new(opts.merge(timeout: timeout, interval: interval)).until { yield }
+        else
+          Timeout.timeout(timeout) do
+            sleep(interval) until yield
+          end
+        end
       end
     end
   end
