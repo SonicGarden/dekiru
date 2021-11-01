233ed2f19d973413ad4cb657f8440c60ca0d0a38
aki <lala.akira@gmail.com>
2021-11-01 13:25:26 +0900

    refactor: jQuery依存のCapybaraヘルパを分割

--- lib/dekiru/capybara/helpers.rb
+++ lib/dekiru/capybara/helpers.rb
@@ -5,41 +5,6 @@ module Dekiru
     module Helpers
       include WaitForPositionStable
 
-      class Error < StandardError; end
-
-      def wait_for_event(event)
-        page.execute_script(<<~"EOS")
-          (function(){
-            var eventName = '#{event}';
-            window._dekiruCapybaraWaitEvents = window._dekiruCapybaraWaitEvents || {};
-            window._dekiruCapybaraWaitEvents[eventName] = 1;
-            jQuery(document).one(eventName, function(){window._dekiruCapybaraWaitEvents[eventName] = 0;});
-          })();
-        EOS
-        yield
-
-        script = <<~"EOS"
-          (function(){
-            var eventName = '#{event}';
-            return window._dekiruCapybaraWaitEvents && window._dekiruCapybaraWaitEvents[eventName];
-          })();
-        EOS
-        wait_until do
-          result = page.evaluate_script(script)
-          raise Error, 'wait_for_event: Missing context. probably moved to another page.' if result.nil?
-          result == 0
-        end
-      end
-
-      # https://robots.thoughtbot.com/automatically-wait-for-ajax-with-capybara
-      def wait_for_ajax
-        wait_until { finished_all_ajax_requests? }
-      end
-
-      def finished_all_ajax_requests?
-        page.evaluate_script('jQuery.active').zero?
-      end
-
       def wait_until(timeout: ::Capybara.default_max_wait_time, interval: 0.2, **opts, &block)
         if defined?(Selenium::WebDriver::Wait)
           Selenium::WebDriver::Wait.new(opts.merge(timeout: timeout, interval: interval)).until { yield }
