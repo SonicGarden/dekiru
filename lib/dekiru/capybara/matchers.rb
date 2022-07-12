fae3b3cb2eb1c53e3b6ee485bc309e64cfe2c891
sincrare <sincrare@sonicgarden.jp>
2022-07-12 16:08:41 +0900

    replaced Manager#logs to Driver#logs Manager#logsが削除されたため

--- lib/dekiru/capybara/matchers.rb
+++ lib/dekiru/capybara/matchers.rb
@@ -3,7 +3,7 @@ module Dekiru
     module Matchers
       class JsNoErrorMatcher
         def matches?(page_or_logs)
-          logs = page_or_logs.respond_to?(:driver) ? page_or_logs.driver.browser.manage.logs.get(:browser) : page_or_logs
+          logs = page_or_logs.respond_to?(:driver) ? page_or_logs.driver.browser.logs.get(:browser) : page_or_logs
           logs.find_all { |log| log.level == 'WARNING' }.each do |log|
             STDERR.puts 'WARN: javascript warning'
             STDERR.puts log.message
