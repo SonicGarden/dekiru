9b0b2fe2878ccbe7993c71daa30e6d884229ef9c
aki <lala.akira@gmail.com>
2022-07-15 14:35:03 +0900

    refactor: ほぼ使われていないヘルパを非推奨に

--- lib/dekiru/helper.rb
+++ lib/dekiru/helper.rb
@@ -1,6 +1,8 @@
 module Dekiru
   module Helper
     def menu_link_to(name = nil, options = nil, html_options = nil, &block)
+      ActiveSupport::Deprecation.warn('`menu_link_to` is deprecated and will be removed in v0.4.')
+
       args = [name, options, html_options]
       html_options, options, name = options, name, block if block_given?
 
@@ -25,12 +27,16 @@ module Dekiru
     alias nl null_check_localization
 
     def facebook_like(url = url_for(only_path: false), app_id = nil, width = 140)
+      ActiveSupport::Deprecation.warn('`menu_link_to` is deprecated and will be removed in v0.4.')
+
       facebook_app_id = app_id || ENV['FACEBOOK_APP_ID']
       html = %Q(<iframe src="//www.facebook.com/plugins/like.php?href=#{u(url)}&amp;send=false&amp;layout=button_count&amp;width=140&amp;show_faces=false&amp;action=like&amp;colorscheme=light&amp;font=arial&amp;height=21&amp;appId=#{facebook_app_id}" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:#{width}px; height:21px;" allowTransparency="true"></iframe>)
       html.html_safe
     end
 
     def twitter_tweet
+      ActiveSupport::Deprecation.warn('`menu_link_to` is deprecated and will be removed in v0.4.')
+
       html = <<-EOF
 <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
@@ -39,6 +45,8 @@ EOF
     end
 
     def google_plus
+      ActiveSupport::Deprecation.warn('`menu_link_to` is deprecated and will be removed in v0.4.')
+
       html = <<-EOF
 <script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
 <g:plusone size="medium"></g:plusone>
@@ -47,6 +55,8 @@ EOF
     end
 
     def google_analytics(code, options = {})
+      ActiveSupport::Deprecation.warn('`menu_link_to` is deprecated and will be removed in v0.4.')
+
       if code.present? && ::Rails.env == 'production'
         multi_subdomain = if options[:domain]
                             "_gaq.push(['_setDomainName', '#{options[:domain]}']);"
