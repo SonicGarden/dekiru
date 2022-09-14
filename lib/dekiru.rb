839926b35b3208411db5d9093200936d09c19436
aki <lala.akira@gmail.com>
2022-09-14 12:53:33 +0900

    test: Remove unused spec

--- lib/dekiru.rb
+++ lib/dekiru.rb
@@ -1,7 +1,6 @@
 require 'dekiru/version'
 require 'dekiru/railtie' if defined?(::Rails)
 require 'dekiru/helper'
-require 'dekiru/controller_additions'
 require 'dekiru/validators/existence'
 require 'dekiru/data_migration_operator'
 require 'dekiru/mail_security_interceptor'
