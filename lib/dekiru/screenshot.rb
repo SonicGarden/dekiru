0670a34c7f19230089a39499bcf3ee414e6fca3f
kei-p <kei-p@sonicgarden.jp>
2022-09-21 10:23:14 +0900

    複数の画面サイズに対応

--- lib/dekiru/screenshot.rb
+++ lib/dekiru/screenshot.rb
@@ -1,15 +1,26 @@
 module Dekiru
   class Screenshot
+    WindowSize = Struct.new(:name, :width, :height, keyword_init: true)
+
     class Configuration
-      attr_accessor :enabled, :root_dir, :window_size
+      attr_accessor :enabled, :root_dir, :window_sizes
 
       def initialize
         @enabled = ENV.has_key?('TAKE_SCREENSHOT')
         @root_dir = Rails.root.join('tmp/screenshots')
-        @window_size = OpenStruct.new(
-          width: 1024,
-          height: 768,
-        )
+        @window_sizes = [
+          WindowSize.new(
+            name: :default,
+            width: 1024,
+            height: 768,
+          )
+        ]
+      end
+
+      def add_multi_sizes(name:, width:, height:)
+        @window_sizes << [
+          WindowSize.new(name: name, width: width, height: height)
+        ]
       end
 
       def enabled?
@@ -35,18 +46,23 @@ module Dekiru
 
     def take(page:, name: nil, suffix: '')
       root_dir = configuration.root_dir
-      file_path = File.join(root_dir, "#{[name, suffix].reject(&:blank?).join('_')}.png")
-      FileUtils.mkdir_p(File.dirname(file_path))
+      file_base_path = File.join(root_dir, "#{[name, suffix].reject(&:blank?).join('_')}.png")
+      FileUtils.mkdir_p(File.dirname(file_base_path))
 
-      width = configuration.window_size.width
-      height = begin
-                 page.evaluate_script('document.querySelector("body").clientHeight')
-               rescue
-                 configuration.window_size.height
-               end
+      configuration.window_sizes.each do |window_size|
+        size_suffix = window_size.name == :default ? '' : window_size.name
+        file_path = File.join(root_dir, "#{[name, suffix, size_suffix].reject(&:blank?).join('_')}.png")
 
-      temporary_resize(page, width, height) do
-        page.save_screenshot(file_path)
+        width = window_size.width
+        height = begin
+                   page.evaluate_script('document.querySelector("body").clientHeight')
+                 rescue
+                   window_size.height
+                 end
+
+        temporary_resize(page, width, height) do
+          page.save_screenshot(file_path)
+        end
       end
     end
 
